1507ef142766c8fb4320519c41a2730287e326c9
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-03-25 23:27:57 -0400

    -

--- Tests/ConnectionTest2.ts
+++ Tests/ConnectionTest2.ts
@@ -1,6 +1,6 @@
 import WebSocket from 'ws';
 
-const ws = new WebSocket('ws://localhost:5555');
+const ws = new WebSocket('ws://localhost:5556');
 
 ws.on('open', () => {
   console.log('connected');
@@ -11,11 +11,47 @@ ws.on('message', (data) => {
 
   switch (ev.event) {
     case 'register': {
-      ws.send(JSON.stringify({ event: 'register', userID: '2' }));
+      ws.send(
+        JSON.stringify({
+          event: 'register',
+          userID: '2',
+          userName: 'ConnectionTest3',
+        })
+      );
       break;
     }
     case 'message': {
       console.log(ev.message);
+      break;
+    }
+    case 'userFound': {
+      console.log(ev.userID);
+      break;
+    }
+    case 'userNotFound': {
+      console.log('User Not Found');
+      break;
     }
   }
 });
+
+setTimeout(() => {
+  ws.send(
+    JSON.stringify({
+      event: 'messageUser',
+      sendTo: '1',
+      message: 'From Client 4',
+      from: '2',
+      date: new Date().toLocaleTimeString(),
+    })
+  );
+}, 1000);
+
+setTimeout(() => {
+  ws.send(
+    JSON.stringify({
+      event: 'findUser',
+      userName: 'mar91248',
+    })
+  );
+}, 2000);
