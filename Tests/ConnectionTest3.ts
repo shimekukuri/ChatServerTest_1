2274c5e8d5e53643f11be25ff08996ba0b5e1a99
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-03-25 23:28:04 -0400

    -

--- Tests/ConnectionTest3.ts
+++ Tests/ConnectionTest3.ts
@@ -11,11 +11,26 @@ ws.on('message', (data) => {
 
   switch (ev.event) {
     case 'register': {
-      ws.send(JSON.stringify({ event: 'register', userID: '4' }));
+      ws.send(
+        JSON.stringify({
+          event: 'register',
+          userID: '4',
+          userName: 'ConnectionTest3',
+        })
+      );
       break;
     }
     case 'message': {
       console.log(ev.message);
+      break;
+    }
+    case 'userFound': {
+      console.log(ev.userID);
+      break;
+    }
+    case 'userNotFound': {
+      console.log('User Not found');
+      break;
     }
   }
 });
@@ -24,10 +39,19 @@ setTimeout(() => {
   ws.send(
     JSON.stringify({
       event: 'messageUser',
-      sendTo: '2341',
-      message: 'From Client 1',
+      sendTo: '640a1e56605518b3b74dc4d1',
+      message: 'From Client 4',
       from: '4',
       date: new Date().toLocaleTimeString(),
     })
   );
-}, 5000);
+}, 1000);
+
+setTimeout(() => {
+  ws.send(
+    JSON.stringify({
+      event: 'findUser',
+      userName: 'mar91248',
+    })
+  );
+}, 2000);
