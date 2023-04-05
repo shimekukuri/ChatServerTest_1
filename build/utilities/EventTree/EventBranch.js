1be491373ccea24307be7f716dd9f6fa9841b167
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-04-05 08:51:22 -0500

    -

--- build/utilities/EventTree/EventBranch.js
+++ build/utilities/EventTree/EventBranch.js
@@ -1,8 +1,21 @@
-export class EventLeaf {
+export class EventBranch {
     val;
+    branches;
     constructor(val) {
         this.val = val;
+        this.branches = [];
     }
+    bfs = (condition) => {
+        let arr = [];
+        for (let i = 0; i < this.branches.length; i++) {
+            if (condition(this.val)) {
+                arr.push(this.branches[i]);
+            }
+        }
+        return arr;
+    };
+    addBranch = () => { };
+    addLeaf = () => { };
 }
-const meep = new EventLeaf({ lol: 'lol' });
+const meep = new EventBranch({ name: 'test', image: '', data: {} });
 console.log(meep.val);
